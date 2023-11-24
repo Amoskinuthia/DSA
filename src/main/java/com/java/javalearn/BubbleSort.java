d960701b501663e6fec78269ef7786e9aba69087
Amoskinuthia <akinuthia@ari.co.ke>
2023-11-24 07:56:48 +0300

    bubble sort

---
+++ src/main/java/com/java/javalearn/BubbleSort.java
@@ -0,0 +1,28 @@
+package com.java.javalearn;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int [] arr = {22,27,15,12,9};
+        System.out.println(Arrays.toString(bubbleSort(arr)));
+    }
+    static int [] bubbleSort(int[] arr){
+        boolean swapped;
+        for (int i = 0; i < arr.length-1; i++) {
+            swapped = false;
+            for (int j = 1; j < arr.length-i; j++) {
+                if (arr[j]>arr[j-1]){
+                    int temp = arr[j];
+                    arr[j]=arr[j-1];
+                    arr[j-1]=temp;
+                    swapped=true;
+                }
+            }
+            if(!swapped){
+                break;
+            }
+        }
+        return arr;
+    }
+}
