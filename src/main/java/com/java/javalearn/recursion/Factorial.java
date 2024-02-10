7cd498c9146d4f52be75c23fa510db7d2020c41f
Amoskinuthia <amoskinuthia15@gamil.com>
2024-02-10 09:51:27 +0300

    factorial using recurssion

--- src/main/java/com/java/javalearn/recursion/Factorial.java
+++ src/main/java/com/java/javalearn/recursion/Factorial.java
@@ -1,4 +1,13 @@
 package com.java.javalearn.recursion;
 
 public class Factorial {
+    public static void main(String [] args){
+        System.out.println(fact(5));
+    }
+    static int fact(int n){
+        if(n<=1){
+            return 1;
+        }
+       return n * fact(n-1);
+    }
 }
